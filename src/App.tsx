14f7bb07c0a5cb462a63898e47e2c95e09d13f99
juancgalueweb <juancgalue@icloud.com>
2023-05-17 18:41:24 -0400

    Inicializando el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Container, Stack, Typography } from '@mui/material'
 import './App.css'
+import { JavaScriptLogo } from './components/JavaScriptLogo'
+import Start from './components/Start'
 
 function App() {
   return (
-    <>
-      <h1>JavaScript Quiz</h1>
-    </>
+    <main>
+      <Container maxWidth='sm'>
+        <Stack
+          direction='row'
+          gap={2}
+          alignItems='center'
+          justifyContent='center'
+        >
+          <JavaScriptLogo />
+          <Typography variant='h2' component='h1'>
+            JavaScript Quiz
+          </Typography>
+        </Stack>
+        <Start />
+      </Container>
+    </main>
   )
 }
 
---
+++ src/components/Start.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@mui/material'
+
+const Start = () => {
+  return <Button variant='contained'>¡Empezar!</Button>
+}
+
+export default Start
